405b93277f89db84e71daf872fc3fffbef78ba83
niharhEAVR <debnathnihar14@gmail.com>
2024-12-01 21:30:00 +0530

    commit

---
+++ class_34/01_sql_relationship/src/01_script.ts
@@ -0,0 +1,39 @@
+import dotenv from 'dotenv';
+import pg from 'pg';
+
+dotenv.config();
+
+const { Client } = pg;
+
+const client = new Client(process.env.POSTGRES_URL)
+
+
+async function main() {
+    try {
+        await client.connect();
+        console.log("Connected to the database");
+    } catch (err) {
+        console.error("Failed to connect to the database", err);
+    }
+
+
+
+
+    const response = await client.query(
+
+        `CREATE TABLE addresses(
+            id SERIAL PRIMARY KEY,
+            user_id INTEGER NOT NULL,
+            city VARCHAR(100) NOT NULL,
+            country VARCHAR(100) NOT NULL,
+            street VARCHAR(255) NOT NULL,
+            pincode VARCHAR(20),
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+        );`
+        
+    )
+
+}
+
+main();
---
+++ class_34/01_sql_relationship/src/02_script.ts
@@ -0,0 +1,40 @@
+// MongoDB client is mongoose
+// and PostGres client is pg
+
+
+import pg from 'pg'
+const { Client } = pg
+
+import express from "express"
+const app = express()
+app.use(express.json())
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const pgClient = new Client(process.env.POSTGRES_URL)
+
+
+async function main() {
+    await pgClient.connect();
+}
+
+main()
+
+
+app.post("/signup", async (req, res) => {
+    const { username, email, password } = req.body;
+    const { city, country, street, pincode } = req.body;
+    
+
+    const insertQuery = `INSERT INTO users (username, email, password) VALUES ($1,$2,$3);`
+    
+    await pgClient.query(insertQuery, [username,email,password]);
+
+    
+    res.status(201).json({ message: "User signed up successfully!" });
+
+})
+
+app.listen(3000)
